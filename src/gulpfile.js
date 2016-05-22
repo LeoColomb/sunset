5d19b42522105f9131ae8ec934e341d20be96da4
Léo Colombaro <git@colombaro.fr>
2016-05-22 13:30:52 +0200

    Sunset :sunrise:

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,6 +1,6 @@
 'use strict';
 const gulp = require('gulp');
-const favicons = require('favicons');
+const favicons = require('gulp-favicons');
 const autoprefixer = require('gulp-autoprefixer');
 const less = require('gulp-less');
 const webserver = require('gulp-webserver');
@@ -9,6 +9,7 @@ const bowerFiles = require('main-bower-files');
 const inject = require('gulp-inject');
 const stripLine = require('gulp-strip-line');
 const livereload = require('gulp-livereload');
+const uglify = require('gulp-uglify');
 
 gulp.task('webserver', function () {
   gulp.src('.')
@@ -37,17 +38,26 @@ gulp.task('css', function () {
 });
 
 gulp.task('icon', function () {
-  favicons({
-    source: 'logo.png',
-    dest: '../'
-  });
+    return gulp.src('logo.png').pipe(favicons({
+        appName: 'Sunset',
+        appDescription: 'A pretty light Mediterranean sunset',
+        developerName: 'Léo Colombaro',
+        developerURL: 'http://colombaro.fr',
+        background: '#f0f0f0',
+        url: 'http://sunset.colombaro.fr',
+        version: 1.0,
+        logging: false,
+        online: false
+    }))
+    .pipe(gulp.dest('..'));
 });
 
 gulp.task('dist', function () {
   gulp.src('index.html')
     .pipe(stripLine([
       '<link href="style.css" rel="stylesheet" />',
-      '<script src="bower_components/goodnight/goodnight.js"></script>'
+      '<script src="bower_components/mojs/build/mojs.min.js"></script>',
+      '<script src="script.js"></script>'
     ]))
     .pipe(inject(gulp.src('style.css'), {
       name: 'style',
@@ -57,6 +67,12 @@ gulp.task('dist', function () {
     }))
     .pipe(inject(gulp.src(bowerFiles()), {
       name: 'bower',
+      transform: function (filePath, file) {
+        return "<script>" + file.contents.toString('utf8') + "</script>";
+      }
+    }))
+    .pipe(inject(gulp.src('script.js').pipe(uglify()), {
+      name: 'script',
       transform: function (filePath, file) {
         return "<script>" + file.contents.toString('utf8') + "</script>";
       }
